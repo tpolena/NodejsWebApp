8f4b77b7765dca337608806ad8ab3924079a9d33
tpolena <tedipolena@gmail.com>
2020-10-15 20:28:07 -0700

    initial commit

---
+++ params/params.js
@@ -0,0 +1,5 @@
+const DATABASECONNECTION = "mongodb+srv://deved:Albania15!@cluster0.hhx45.mongodb.net/test?retryWrites=true&w=majority"
+
+module.exports = {
+    DATABASECONNECTION
+}
---
+++ routes/web/index.js
@@ -0,0 +1,15 @@
+
+var express = require("express");
+
+var router = express.Router();
+
+router.use(function(req,res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
+
+
+router.use("/", require("./home"));
+
+
+module.exports = router;
