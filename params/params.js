f2e23a5a2f22ed21a751eae46cfe2de521791e82
tpolena <30359040+tpolena@users.noreply.github.com>
2020-10-15 20:37:02 -0700

    Update params.js

--- params/params.js
+++ params/params.js
@@ -1,4 +1,4 @@
-const DATABASECONNECTION = "mongodb+srv://deved:Albania15!@cluster0.hhx45.mongodb.net/test?retryWrites=true&w=majority"
+const DATABASECONNECTION = "mongodb+srv://deved:Helloworld123@cluster0.hhx45.mongodb.net/test?retryWrites=true&w=majority"
 
 module.exports = {
     DATABASECONNECTION
